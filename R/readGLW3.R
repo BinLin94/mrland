90f25ccfe891fbcc02676f471db727d5608af72c
Bin Lin <aviva_lin@126.com>
2026-06-29 13:58:01 +0200

    readGLW3/downloadGLW3: add 16 species subtypes with backward compatibility
    Add Da/Aw subtypes for 8 species (Ct, Sh, Pg, Bf, Ch, Ho, Gt, Dk)
    for both dasymetric and areal weighting. readGLW3 retains legacy Da
    and Aw subtypes for backward compatibility with existing callers.

--- R/readGLW3.R
+++ R/readGLW3.R
@@ -1,38 +1,71 @@
 #' @title readGLW3
-#' @description Read the gridded livestock of the world 3 dataset.
-#' @param subtype Subtype of file to be opened (either Da or Aw)
-#' @return Magpie objects
-#' @author Marcos Alves
+#' @description Reads Gridded Livestock of the World version 3 (GLW 3)
+#'   raster data for reference year 2010, downloaded from Harvard Dataverse.
+#'   Eight livestock species are available with both dasymetric and areal
+#'   weighting.
+#'   Source catalogue:
+#'   https://www.fao.org/livestock-systems/global-distributions/en/
+#' @param subtype Weighting method, livestock species, and reference year
+#'   (\code{"<method>_<species>_2010"}):
+#'   \itemize{
+#'     \item {Da}: Dasymetric weighting informed by Random Forest
+#'     \item {Aw}: Areal weighting (distributed uniformly in each census unit)
+#'     \itemize{
+#'       \item \code{Ct}: Cattle
+#'       \item \code{Sh}: Sheep
+#'       \item \code{Pg}: Pigs
+#'       \item \code{Bf}: Buffaloes
+#'       \item \code{Ch}: Chickens
+#'       \item \code{Ho}: Horses
+#'       \item \code{Gt}: Goats
+#'       \item \code{Dk}: Ducks
+#'     }
+#'   }
+#' @return A magpie object with 67420 lpjcell coordinates, one year (y2010),
+#'   and gridded livestock counts in heads per 0.5-degree pixel.
+#' @author Marcos Alves, Bin Lin
+#' @seealso \code{\link{readGLW4}}, \code{\link{correctGLW3}}
 #' @examples
 #' \dontrun{
-#' readSource("GLW3", subtype = "DA", convert = "onlycorrect")
+#' readSource("GLW3", subtype = "Da_Ct_2010", convert = FALSE)
 #' }
-#'
-#' @import madrat
-#' @importFrom raster aggregate raster rasterToPoints
-#' @importFrom dplyr left_join
+#' @importFrom terra rast aggregate extract
+#' @importFrom madrat toolSubtypeSelect
+#' @importFrom mstools toolGetMappingCoord2Country
+#' @importFrom magclass as.magpie getYears<- getNames<-
 
-readGLW3 <- function(subtype = "Da") {
+readGLW3 <- function(subtype = "Da_Ct_2010") {
 
-    strName <- toolSubtypeSelect(subtype, c(Da = "5_Ct_2010_Da.tif",
-                                            Aw = "6_Ct_2010_Aw.tif"))
-    x <- raster(strName)
-    x <- raster::aggregate(x, fact = 6, fun = sum, na.rm = TRUE)
-    x <- rasterToPoints(x)
+  file <- toolSubtypeSelect(subtype, c(
+    Da_Ct_2010 = "5_Ct_2010_Da.tif",
+    Aw_Ct_2010 = "6_Ct_2010_Aw.tif",
+    Da_Sh_2010 = "5_Sh_2010_Da.tif",
+    Aw_Sh_2010 = "6_Sh_2010_Aw.tif",
+    Da_Pg_2010 = "5_Pg_2010_Da.tif",
+    Aw_Pg_2010 = "6_Pg_2010_Aw.tif",
+    Da_Bf_2010 = "5_Bf_2010_Da.tif",
+    Aw_Bf_2010 = "6_Bf_2010_Aw.tif",
+    Da_Ch_2010 = "5_Ch_2010_Da.tif",
+    Aw_Ch_2010 = "6_Ch_2010_Aw.tif",
+    Da_Ho_2010 = "5_Ho_2010_Da.tif",
+    Aw_Ho_2010 = "6_Ho_2010_Aw.tif",
+    Da_Gt_2010 = "5_Gt_2010_Da.tif",
+    Aw_Gt_2010 = "6_Gt_2010_Aw.tif",
+    Da_Dk_2010 = "5_Dk_2010_Da.tif",
+    Aw_Dk_2010 = "6_Dk_2010_Aw.tif"
+  ))
 
-    colnames(x) <- c("lon", "lat", paste0("X5_Ct_2010_", subtype))
+  r <- rast(file)
+  r <- aggregate(r, fact = 6, fun = sum, na.rm = TRUE)
 
-    # mapping with coordinates of 67420 grid cells
-    mapping          <- toolGetMappingCoord2Country()
-    mapping$coordiso <- paste(mapping$coords, mapping$iso, sep = ".")
-    mapping$lat      <- as.numeric(gsub("p", ".", gsub(".*\\.", "", mapping$coords)))
-    mapping$lon      <- as.numeric(gsub("p", ".", gsub("\\..*", "", mapping$coords)))
-
-    # reduce number of cells and transform to magpie object
-    x <- left_join(mapping, x, by = c("lat", "lon"), copy = TRUE)
-    x <- as.magpie(x[, c(3, 6)], spatial = 1)
-    getItems(x, dim = 1, raw = TRUE) <- mapping$coordiso
-    getSets(x) <- c("x", "y", "iso", "year", "data")
-
-    return(x)
-  }
+  map <- toolGetMappingCoord2Country(pretty = TRUE)
+  x <- as.magpie(extract(r, map[c("lon", "lat")], ID = FALSE), spatial = 1)
+  dimnames(x) <- list(
+    "x.y.iso" = paste(map$coords, map$iso, sep = "."),
+    "t"        = NULL,
+    "data"     = subtype
+  )
+  getYears(x) <- "y2010"
+  attr(x, "unit") <- "heads/pixel"
+  return(x)
+}
